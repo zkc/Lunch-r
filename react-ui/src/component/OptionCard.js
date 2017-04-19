bc02346ed691aea268994c221c9c3367caf0c7d2
Kyle Zucker <zkyle12@gmail.com>
2017-04-18 18:08:36 -0600

    move on-click to entire option-card

--- react-ui/src/component/OptionCard.js
+++ react-ui/src/component/OptionCard.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const OptionCard = ({ location, updateChoice, isSelected, voteTotal }) => {
   return (
-    <div className={ `option-card ${isSelected ? 'selected' : ''}` }>
+    <div className={ `option-card ${isSelected ? 'selected' : ''}` } onClick={ () => updateChoice(location) }>
       <p className="option-vote-total" > { voteTotal } </p>
       <p className="option-location"> { location } </p>
-      <div className="option-vote-button" onClick={ () => updateChoice(location) }>
+      <div className="option-vote-button">
         <img src="https://s3-us-west-1.amazonaws.com/zuck-bucket/tick.png" />
       </div>
     </div>
