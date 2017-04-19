45a0efdda4bbdf77a782264b40e40bf5f4b5ad88
Kyle Zucker <zkyle12@gmail.com>
2017-04-19 13:51:56 -0600

    add big new group button to /

--- react-ui/src/component/App.js
+++ react-ui/src/component/App.js
@@ -25,6 +25,11 @@ class App extends Component {
           </Link>
         </div>
         <div className="App-intro">
+          <Route exact path={'/'} render={() =>
+            <Link className="new-group-welcome" to="/new">
+              Start a new Group
+            </Link>
+          } />
           <Route path={'/join/:group_id'} render={({ match }) => <VotePage socket={socket} user_nug={user_nug} group_id={ match.params.group_id } />} />
           <Route exact path={'/new'} render={({ match }) => <NewGroup socket={socket} history={this.props.history} group_id={ match.params.group_id } />} />
           <Route path={'/new/:group_id'} render={({ match }) => <NewGroup socket={socket} group_id={ match.params.group_id } />} />
